71a35762aae9e47a9c61bd90d3af4587bd40d97e
lauraporta <ucqflpo@ucl.ac.uk>
2024-11-18 20:57:07 +0000

    Add rotating plane

---
+++ rotating_plane.js
@@ -0,0 +1,15 @@
+// init p5
+p5 = new P5({mode: 'WEBGL'})
+  
+// as in setup - static
+
+// you can then draw
+p5.draw = () => {
+  p5.push();
+  p5.translate(0, 0, 0);
+  p5.rotateZ(p5.frameCount * 0.01);
+  p5.rotateX(p5.frameCount * 0.01);
+  p5.rotateY(p5.frameCount * 0.01);
+  p5.plane(500); // increase the size for visibility
+  p5.pop();
+}
